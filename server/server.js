fc884174529118780161c65457874c5add0765b1
saheb <sahibbhalla.010@gmail.com>
2022-11-07 19:04:03 -0500

    Added webpack and heroku

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 require('./routes/htmlRoutes.js')(app);
 const PORT = process.env.PORT || 3001;
-
+app.use(express.static('../client/dist/'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
-app.use(express.static('../client/'))
+
 app.listen(PORT,function(){
     console.log(`Now Listening on porta: ${PORT}`)
 });
